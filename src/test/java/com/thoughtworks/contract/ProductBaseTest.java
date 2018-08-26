6ed1909f0c65dfbdd3a9e117f0468d32064475a0
FeiXie-Liam <xiefei0887@163.com>
2018-08-26 23:41:35 +0800

    [Fei XIE] test : add DB-Rider to generate database data for test

--- src/test/java/com/thoughtworks/contract/ProductBaseTest.java
+++ src/test/java/com/thoughtworks/contract/ProductBaseTest.java
@@ -1,5 +1,8 @@
 package com.thoughtworks.contract;
 
+import com.github.database.rider.core.api.configuration.DBUnit;
+import com.github.database.rider.core.api.dataset.DataSet;
+import com.github.database.rider.spring.api.DBRider;
 import com.thoughtworks.contract.controller.ProductController;
 import io.restassured.module.mockmvc.RestAssuredMockMvc;
 import org.junit.Before;
@@ -8,14 +11,17 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.cloud.contract.verifier.messaging.boot.AutoConfigureMessageVerifier;
 import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.test.web.servlet.setup.StandaloneMockMvcBuilder;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(classes = ContractApplication.class, webEnvironment = SpringBootTest.WebEnvironment.MOCK)
-@DirtiesContext
-@AutoConfigureMessageVerifier
+@DBRider
+@ActiveProfiles("test")
+@DBUnit(caseSensitiveTableNames = true)
+@DataSet("product.yml")
 public abstract class ProductBaseTest {
     @Autowired
     private ProductController productController;
